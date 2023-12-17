75a1a324f9f85f85acbdbb18a4060270cbf68746
huMustafa <151642572+huMustafa@users.noreply.github.com>
2023-12-17 21:42:30 +0500

    Adding Comments

--- cursor.cpp
+++ cursor.cpp
@@ -1,9 +1,10 @@
 #pragma once
 #include "cursor.hpp"
 
-Cursor::Cursor(Grid *grid_to_use, grid_mode mode_to_use, int w, int h){
+// Constructor for Cursor class
+Cursor::Cursor(Grid *grid_to_use, grid_mode mode_to_use, int w, int h) {
     current_grid = grid_to_use;
-    mode = mode_to_use,
+    mode = mode_to_use;
     width = w;
     height = h;
     current_ship = CARRIER;
@@ -11,30 +12,37 @@ Cursor::Cursor(Grid *grid_to_use, grid_mode mode_to_use, int w, int h){
     set_init_position();
 }
 
-void Cursor::set_init_position(void){
-    for (int y = 1; y < GRID_HEIGHT; y++){
-        for (int x = 1; x < GRID_WIDTH; x++){
-            if (fits_in_coordinate(x, y)){
+// Set initial position of the cursor
+void Cursor::set_init_position(void) {
+    for (int y = 1; y < GRID_HEIGHT; y++) {
+        for (int x = 1; x < GRID_WIDTH; x++) {
+            if (fits_in_coordinate(x, y)) {
                 c_position = {x, y}; 
+
+                // Set screen position based on mode
                 if (mode == RADAR_MODE)
                     screen_position = {0, 0, width * TILE_SIZE, height * TILE_SIZE};
-                else{
-                    screen_position = {0, 0, width * TILE_SIZE, height * TILE_SIZE};} 
+                else
+                    screen_position = {0, 0, width * TILE_SIZE, height * TILE_SIZE}; 
+
+                // Set ship position based on cursor position
                 set_ship_position();
-                x = 100;
-                y = 100;
+                x = 100;  // Break outer loop
+                y = 100;  // Break inner loop
             }
         }
     }
 }
 
-void Cursor::set_texture(SDL_Texture *texture){
+// Set texture for rendering
+void Cursor::set_texture(SDL_Texture *texture) {
     c_texture = texture;
 }
 
-bool Cursor::tile_is_free(int x, int y){
-    if (x < 11 && x > 0 && y < 11 && y > 0){
-        switch (mode){
+// Check if a tile is free based on the cursor mode
+bool Cursor::tile_is_free(int x, int y) {
+    if (x < 11 && x > 0 && y < 11 && y > 0) {
+        switch (mode) {
             case RADAR_MODE:
                 return !current_grid->tile_is_bombed(x, y);
 
@@ -46,27 +54,28 @@ bool Cursor::tile_is_free(int x, int y){
     return false;
 }
 
-bool Cursor::fits_in_coordinate(int x, int y){
-    for (int horizontal_check = 0; horizontal_check < width; horizontal_check++){
-        if(!tile_is_free(x + horizontal_check, y))
+// Check if the cursor fits in a given coordinate
+bool Cursor::fits_in_coordinate(int x, int y) {
+    for (int horizontal_check = 0; horizontal_check < width; horizontal_check++) {
+        if (!tile_is_free(x + horizontal_check, y))
             return false;
     }
-    for (int vertical_check = 0; vertical_check < height; vertical_check++){
-        if(!tile_is_free(x, y + vertical_check))
+    for (int vertical_check = 0; vertical_check < height; vertical_check++) {
+        if (!tile_is_free(x, y + vertical_check))
             return false;
     }
     return true;
 }
 
-bool Cursor::move_up(void){
-    if (c_position.y > 1){
-        if (mode == OCEAN_MODE){
-            if (fits_in_coordinate(c_position.x, c_position.y - 1)){
+// Move the cursor up
+bool Cursor::move_up(void) {
+    if (c_position.y > 1) {
+        if (mode == OCEAN_MODE) {
+            if (fits_in_coordinate(c_position.x, c_position.y - 1)) {
                 c_position.y -= 1;
                 return true;
             }
-        }
-        else{
+        } else {
             c_position.y -= 1;
             return true;
         }
@@ -74,15 +83,15 @@ bool Cursor::move_up(void){
     return false;
 }
 
-bool Cursor::move_down(void){
-    if (c_position.y < GRID_HEIGHT){
-        if (mode == OCEAN_MODE){
-            if (fits_in_coordinate(c_position.x, c_position.y + 1)){
+// Move the cursor down
+bool Cursor::move_down(void) {
+    if (c_position.y < GRID_HEIGHT) {
+        if (mode == OCEAN_MODE) {
+            if (fits_in_coordinate(c_position.x, c_position.y + 1)) {
                 c_position.y += 1;
                 return true;
             }
-        }
-        else{
+        } else {
             c_position.y += 1;
             return true;
         }
@@ -90,15 +99,15 @@ bool Cursor::move_down(void){
     return false;
 }
 
-bool Cursor::move_right(void){
-    if (c_position.x < GRID_WIDTH){
-        if(mode == OCEAN_MODE){
-            if (fits_in_coordinate(c_position.x + 1, c_position.y)){
+// Move the cursor right
+bool Cursor::move_right(void) {
+    if (c_position.x < GRID_WIDTH) {
+        if (mode == OCEAN_MODE) {
+            if (fits_in_coordinate(c_position.x + 1, c_position.y)) {
                 c_position.x += 1;
                 return true;
             }
-        }
-        else{
+        } else {
             c_position.x += 1;
             return true;
         }
@@ -106,15 +115,15 @@ bool Cursor::move_right(void){
     return false;
 }
 
-bool Cursor::move_left(void){
-    if (c_position.x > 1){
-        if(mode == OCEAN_MODE){
-            if (fits_in_coordinate(c_position.x - 1, c_position.y)){
+// Move the cursor left
+bool Cursor::move_left(void) {
+    if (c_position.x > 1) {
+        if (mode == OCEAN_MODE) {
+            if (fits_in_coordinate(c_position.x - 1, c_position.y)) {
                 c_position.x -= 1;
                 return true;
             }
-        }
-        else{
+        } else {
             c_position.x -= 1;
             return true;
         }
@@ -122,9 +131,10 @@ bool Cursor::move_left(void){
     return false;
 }
 
-ship_type Cursor::next_ship(void){
+// Switch to the next ship type and set initial position
+ship_type Cursor::next_ship(void) {
     current_ship_orientation = UP;
-    switch (current_ship){
+    switch (current_ship) {
         case CARRIER:
             current_ship = BATTLESHIP;
             width = 1;
@@ -160,10 +170,11 @@ ship_type Cursor::next_ship(void){
     return current_ship;
 }
 
-bool Cursor::toggle_ship_orientation(void){
+// Toggle ship orientation and adjust position if valid
+bool Cursor::toggle_ship_orientation(void) {
     swap_dimensions();
-    if (fits_in_coordinate(c_position.x, c_position.y)){
-        switch (current_ship_orientation){
+    if (fits_in_coordinate(c_position.x, c_position.y)) {
+        switch (current_ship_orientation) {
             case UP:
                 current_ship_orientation = RIGHT;
                 break;
@@ -181,20 +192,20 @@ bool Cursor::toggle_ship_orientation(void){
                 break;
         }
         return true;
-    }
-    else{
+    } else {
         swap_dimensions();
         return false;
     }
 }
 
-void Cursor::set_ship_position(void){
+// Set ship position in the grid
+void Cursor::set_ship_position(void) {
     current_grid->set_ship_position(current_ship, c_position.x, c_position.y, current_ship_orientation);
 }
 
-void Cursor::set_screen_position(void){
-    switch (mode)
-    {
+// Set screen position based on cursor mode
+void Cursor::set_screen_position(void) {
+    switch (mode) {
         case RADAR_MODE:
             screen_position.x = TILES_RADAR_INIT_X_POS + ((c_position.x - 1) * TILE_SIZE);
             screen_position.y = TILES_RADAR_INIT_Y_POS + ((c_position.y - 1) * TILE_SIZE);
@@ -206,28 +217,32 @@ void Cursor::set_screen_position(void){
             screen_position.y = TILES_OCEAN_INIT_Y_POS + ((c_position.y - 1) * TILE_SIZE);
             break;
     }
-    
 }
 
-void Cursor::draw(SDL_Renderer *renderer){
+// Render the cursor
+void Cursor::draw(SDL_Renderer *renderer) {
     set_screen_position();
     SDL_RenderCopy(renderer, c_texture, &TILE_SPRITES.green, &screen_position);
 }
 
-void Cursor::swap_dimensions(void){
+// Swap cursor dimensions
+void Cursor::swap_dimensions(void) {
     int temp = width;
     width = height;
     height = temp;
 }
 
-ship_type Cursor::current_ship_in_use(void){
+// Get the current ship type in use
+ship_type Cursor::current_ship_in_use(void) {
     return current_ship;
 }
 
-tile_state Cursor::try_bomb_enemy(void){
+// Try to bomb the enemy and get the tile state
+tile_state Cursor::try_bomb_enemy(void) {
     return current_grid->bomb_tile(c_position.x, c_position.y);
 }
 
-void Cursor::assign_ship_position(void){
+// Assign ship position to tiles in the grid
+void Cursor::assign_ship_position(void) {
     current_grid->assign_ship_to_tiles(current_ship, c_position.x, c_position.y, current_ship_orientation);
 }
